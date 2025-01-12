d3191b294ad98eac5c6256c9c0f4048fe839586e
Manisha <manisha.sapra92@gmail.com>
2025-01-12 15:17:49 +0530

    12th Jan 2025

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,19 @@
+package api.endpoints;
+
+/* swagger url: https://petstore.swagger.io/
+ * create user (Post): https://petstore.swagger.io/#/user/createUser
+ * Get user (Get): https://petstore.swagger.io/#/user/getUserByName
+ * Update user (Put) : https://petstore.swagger.io/#/user/updateUser
+ * Delete user (Delete): https://petstore.swagger.io/#/user/deleteUser
+ */
+
+public class Routes {
+ public static String base_url = "https://petstore.swagger.io/v2" ;
+ 
+ 
+//user module
+ public static String post_url = "base_url" + "/user";
+ public static String get_url = "base_url" + "/user/{username}";
+ public static String put_url = "base_url" + "/user/{username}";
+ public static String delete_url = "base_url" + "/user/{username}";
+}
