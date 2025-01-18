9a062953759210932d26afa907257f715347c1df
Manisha <manisha.sapra92@gmail.com>
2025-01-18 18:52:43 +0530

    18th Jan 2025

--- src/test/java/api/endpoints/StoreEndPoints.java
+++ src/test/java/api/endpoints/StoreEndPoints.java
@@ -16,5 +16,14 @@ public class StoreEndPoints {
 		
 		return response;
 	}
+	public static Response deleteOrder(Integer id) {
+		Response response = given()
+				.pathParam("orderid", id)
+		.when()
+		.get(Routes.deletestore_url);
+		
+		
+		return response;
+	}
 
 }
