727a8efbb17d90ff078154b684a7a6528eaef4d6
Manisha <manisha.sapra92@gmail.com>
2025-01-19 10:15:13 +0530

    19th Jan 2025

--- src/test/java/api/endpoints/StoreEndPoints.java
+++ src/test/java/api/endpoints/StoreEndPoints.java
@@ -2,7 +2,8 @@ package api.endpoints;
 
 import static io.restassured.RestAssured.given;
 
-
+import api.payload.Store;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class StoreEndPoints {
@@ -21,6 +22,17 @@ public class StoreEndPoints {
 				.pathParam("orderid", id)
 		.when()
 		.get(Routes.deletestore_url);
+		
+		
+		return response;
+	}
+	public static Response postOrder(Store payload){
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(payload)
+		.when()
+		.post(Routes.postorder_url);
 		
 		
 		return response;
---
+++ src/test/java/api/payload/Store.java
@@ -0,0 +1,47 @@
+package api.payload;
+
+public class Store {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPetId() {
+		return petId;
+	}
+	public void setPetId(int petId) {
+		this.petId = petId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getShipDate() {
+		return shipDate;
+	}
+	public void setShipDate(String shipDate) {
+		this.shipDate = shipDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getComplete() {
+		return complete;
+	}
+	public void setComplete(String complete) {
+		this.complete = complete;
+	}
+	int id;
+	int petId;
+	int quantity;
+	String shipDate;
+	String status;
+	String complete;
+
+}
